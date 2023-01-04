c7e1811adaca6d9d392eaea6f04ba28221123154
howooking <junsgk@gmail.com>
2023-01-04 16:45:32 +0900

    vercel auto deploy

--- components/date.js
+++ components/date.js
@@ -2,6 +2,5 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  console.log("good");
   return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 }
---
+++ posts/howoo.md
@@ -0,0 +1,7 @@
+---
+title: "Who is Howoo?"
+date: "2020-03-01"
+---
+
+howoo is cutest cat on the Earth!.
+If you deny this, you are a foolish.
