02cf6ceb81535ff691bc443e391082a3ef279e34
howooking <junsgk@gmail.com>
2023-01-04 16:33:45 +0900

    lowercase...

--- components/Date.jsx
+++ components/Date.jsx
@@ -2,5 +2,6 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
+  console.log("good");
   return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
 }
